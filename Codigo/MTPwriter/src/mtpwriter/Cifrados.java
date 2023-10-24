6af7c8cb79d5edb61795978abb3663d33fc384f7
nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2023-10-23 19:21:31 -0600

    Writer 2.0

--- Codigo/MTPwriter/src/mtpwriter/Cifrados.java
+++ Codigo/MTPwriter/src/mtpwriter/Cifrados.java
@@ -3,27 +3,54 @@ package mtpwriter;
 import java.util.Random;
 
 public class Cifrados {
-
+    
+    Aleatorios aleatorio;
     StringBuilder builder;
     Random random;
     
+    char arrayMensaje[];
+    
     public String primerCifrado(String mensaje,int frecuencia,int cantidad) {
         
+        String cifrado;
+        aleatorio = new Aleatorios();
         builder = new StringBuilder();
         random = new Random();
         
         int contador = 0;
         
-        for (int i = 0; i < frase.length(); i++) {
-            resultado.append(frase.charAt(i));
+        for (int i = 0; i < mensaje.length(); i++) {
             
-            if (++contador == primerNumero) {
-                for (int j = 0; j < segundoNumero; j++) {
-                    char caracterAleatorio = generarCaracterAleatorio(random);
-                    resultado.append(caracterAleatorio);
+            builder.append(mensaje.charAt(i));
+            
+            if (++contador == frecuencia) {
+                
+                for (int j = 0; j < cantidad; j++) {
+                    
+                    char randomChar = aleatorio.basuraRandom(random);
+                    builder.append(randomChar);
+                    
                 }
                 contador = 0;
             }
         }
+        
+        cifrado = builder.toString();
+        return cifrado;
     }
+    
+    public String segundoCifrado(String cifrado,int desplazamiento) {
+        
+        arrayMensaje = cifrado.toCharArray();
+        
+        for (int i = 0; i < arrayMensaje.length; i++) {
+            
+            arrayMensaje[i] = (char)(arrayMensaje[i] + (char)desplazamiento);
+            
+        }
+        
+        cifrado = String.valueOf(arrayMensaje);
+        return cifrado;
+    }
+
 }
--- Codigo/MTPwriter/src/mtpwriter/MTPwriter.java
+++ Codigo/MTPwriter/src/mtpwriter/MTPwriter.java
@@ -8,79 +8,48 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MTPwriter {  
+    
     public static void main(String[] args) {
         
         int frecuencia;
         int cantidad;
         int desplazamiento;
+        
         String mensaje;
-        String cifrado1;
-        String cifrado2;
+        String cifrado;
+        String archivo;
         
         Serializa serial = new Serializa();
         Aleatorios aleatorio = new Aleatorios();
+        Cifrados cifrador = new Cifrados();
         
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Ingresa una frase (solo letras, números y signos de puntuación básicos): ");
-        mensaje = scanner.nextLine();
         
         desplazamiento = aleatorio.numeroRandom();
         frecuencia = aleatorio.numeroRandom();
         cantidad = aleatorio.numeroRandom();
         
-        cifrado1 = encriptarFrase(mensaje,frecuencia,cantidad);
-        
-        
-        
-        mensaje = cifrado1;
+        System.out.print("Ingresa una frase (solo letras, números y signos de puntuación básicos): ");
+        mensaje = scanner.nextLine();
         
         while (!mensaje.matches("^[a-zA-Z0-9 ]*$")) {
             System.out.println("Por favor ingrese un mensaje que solo contenga números y letras.");
             mensaje = scanner.nextLine();
         }
-
-        char desface[] = mensaje.toCharArray();
-
-        for (int i = 0; i < desface.length; i++){
-            desface[i] = (char)(desface[i] + (char)numeroD);
-        }
-        String encriptado = String.valueOf(desface);
-
-        String textoASerializar = encriptado;
         
+        cifrado = cifrador.primerCifrado(mensaje, frecuencia, cantidad);
+        
+        cifrado = cifrador.segundoCifrado(cifrado, desplazamiento);
+
         System.out.println("Ingresa el nombre del archivo");
-        String archivo = scanner.nextLine();
-        String nombreDelArchivo =  archivo + ".txt";
+        archivo = scanner.nextLine();
+        archivo =  archivo + ".txt";
         
-        serial.serializar(textoASerializar, nombreDelArchivo);
+        serial.serializar(cifrado,archivo);
         
-        System.out.println("Primer número aleatorio generado: " + primerNumeroAleatorio);
-        System.out.println("Segundo número aleatorio generado: " + segundoNumeroAleatorio);
-        System.out.println("Segundo número aleatorio generado: " + numeroD);
-        System.out.println("Frase encriptada: " + encriptado);
-        System.out.println("Clave: " + primerNumeroAleatorio + segundoNumeroAleatorio + numeroD);
+        System.out.println("Frase encriptada: " + cifrado);
+        System.out.println("Nombre del archivo: " + archivo);
+        System.out.println("Clave secreta: " + frecuencia + cantidad + desplazamiento);
     }
     
-    public static String encriptarFrase(String frase, int primerNumero, int segundoNumero) {
-        StringBuilder resultado = new StringBuilder();
-        Random random = new Random();
-        
-        int contador = 0;
-        
-        for (int i = 0; i < frase.length(); i++) {
-            resultado.append(frase.charAt(i));
-            
-            if (++contador == primerNumero) {
-                for (int j = 0; j < segundoNumero; j++) {
-                    char caracterAleatorio = generarCaracterAleatorio(random);
-                    resultado.append(caracterAleatorio);
-                }
-                contador = 0;
-            }
-        }
-        
-        return resultado.toString();
-    }
-   
 }
